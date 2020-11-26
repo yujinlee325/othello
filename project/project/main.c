670c38af41f136a35e1154f1b38e22e06f3126d3
yujinlee325 <yu_jin325@naver.com>
2020-11-26 23:04:05 +0900

    othello

--- project/project/main.c
+++ project/project/main.c
@@ -2,58 +2,72 @@
 
 #define N 8
 
-int main(){
+char* gameboard[N][N];
+
+void init_othello();
+void print_othello();
+
+void init_othello(){
+  
     int i, j;
-    char* arr[N][N];
-    char anbr[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    for(i=0;i<N+1;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+                if(i == 0 && 1 <= j <= N-2) //판을 그리기 위해 각 배열에 알맞은 문자 입력
+                    gameboard[i][j] = "┬";
+                else if (i == 0 && j == 0)
+                    gameboard[i][j] = "┌";
+                else if (i == 0 && j == N-1)
+                    gameboard[i][j] = "┐";
+                else if(1 <= i <= 6 && j == 0)
+                    gameboard[i][j] = "├" ;
+                else if (i == N-1 && j == 0)
+                    gameboard[i][j] = "└";
+                else if (i == N-1 && 1 <= j <= N-2)
+                    gameboard[i][j] = "┴";
+                else if (i == N-1 && j == N-1)
+                  gameboard[i][j] = "┘";
+                else if (1 <= i <= N-2 && j == N-1)
+                  gameboard[i][j] = "┤";
+                else if((i == N/2-1 && j == N/2-1) || (i == N/2 && j == N/2)) //초기 말 배치
+                    gameboard[i][j] = "○" ;
+                else if ((i == N/2-1 && j == N/2) || (i == N/2 && j == N/2-1))
+                    gameboard[i][j] = "●";
+                else
+                    gameboard[i][j] = "┼";
+            }
+    }
+}
+void print_othello(){
+    int i,j;
+    char anbr[26] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     printf(" ");
     
     for(i=1;i<N+1;i++)
         printf("%d", i);
-    
-        printf("\n");
-    
-    for(i=0;i<N;i++)
-    {
-            for(j=0;j<N;j++)
-            {
-                if(i == 0 && 1 <= j <= N-2)
-                    arr[i][j] = "┬";
-                else if (i == 0 && j == 0)
-                    arr[i][j] = "┌";
-                else if (i == 0 && j == N-1)
-                    arr[i][j] = "┐";
-                else if(1 <= i <= 6 && j == 0)
-                    arr[i][j] = "├" ;
-                else if (i == N-1 && j == 0)
-                    arr[i][j] = "└";
-                else if (i == N-1 && 1 <= j <= N-2)
-                    arr[i][j] = "┴";
-                else if (i == N-1 && j == N-1)
-                    arr[i][j] = "┘";
-                else if (1 <= i <= N-2 && j == N-1)
-                    arr[i][j] = "┤";
-                else if((i == N/2-1 && j == N/2-1) || (i == N/2 && j == N/2))
-                    arr[i][j] = "○" ;
-                else if ((i == N/2-1 && j == N/2) || (i == N/2 && j == N/2-1))
-                    arr[i][j] = "●";
-                else
-                    arr[i][j] = "┼";
-            }
-    }
+    printf("\n");
     
     for(i=0;i<N;i++)
     {
-        printf("%c", anbr[i]);
+            printf("%c", anbr[i]);
         
         for(j=0;j<N;j++)
-            printf("%s", arr[i][j]);
+            printf("%s", gameboard[i][j]);
         
         printf("\n");
     }
-    return 0;
 }
+            
+void main()
+{
+    init_othello();
+    print_othello();
+}
+
+
                     
                     
                     
